bdb3191610c85effd70def915d1be68930b5aa3a
Loris Bognanni <loris@codemade.co.uk>
2020-12-04 09:28:27 +0000

    Day 4 step 1

---
+++ Day4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    class Program
+    {
+
+        static int GetNumberOfValidPassports()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            int validPassports = 0;
+            int currentPassportFields = 0;
+
+            string[] requiredFields = new[]
+            {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid",
+            };
+
+            void UpdateCount()
+            {
+                if (currentPassportFields == requiredFields.Length)
+                {
+                    validPassports++;
+                }
+                currentPassportFields = 0;
+            }
+
+            foreach (var line in lines)
+            {
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    UpdateCount();
+                    continue;
+                }
+                var fieldsAndValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach(var fieldWithValue in fieldsAndValues)
+                {
+                    var split = fieldWithValue.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                    if(requiredFields.Contains(split[0]))
+                    {
+                        currentPassportFields++;
+                    }
+                }
+            }
+            UpdateCount();
+
+            return validPassports;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Found {GetNumberOfValidPassports()} valid passports.");
+        }
+    }
+}
