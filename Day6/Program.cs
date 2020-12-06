06142d5073a716520a7d2fb4262a36542ddf3180
Loris Bognanni <loris@codemade.co.uk>
2020-12-06 11:06:11 +0000

    Day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -14,18 +14,75 @@ namespace Day6
             var lines = File.ReadAllLines("input.txt");
             var groups = ReadGroupAnswers(lines);
 
-            var count = groups.Select(g => g.Distinct().Count()).Sum();
+            var part1Count = groups.Select(g => g.Distinct().Count()).Sum();
 
-            Console.WriteLine($"The total count is {count}");
+            Console.WriteLine($"Part 1: The total count is {part1Count}");
+
+            var exampleLines = new[]
+            {
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b",
+            };
+
+
+            Console.WriteLine($"Example count is {ReadGroupAnswersPart2(exampleLines).Sum()}");
+
+            var part2Count = ReadGroupAnswersPart2(lines).Sum();
+
+            Console.WriteLine($"Part 2: The total count is {part2Count}");
+        }
+
+        private static IEnumerable<int> ReadGroupAnswersPart2(string[] lines)
+        {
+            var allAnswers = new List<char>();
+            var newGroup = true;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    yield return allAnswers.Count;
+                    allAnswers = new List<char>();
+                    newGroup = true;
+                    continue;
+                }
+
+                if (newGroup)
+                {
+                    newGroup = false;
+                    allAnswers.AddRange(line.ToCharArray());
+                }
+                else
+                {
+                    allAnswers = allAnswers.Intersect(line).ToList();
+                }
+            }
+            if (allAnswers.Any())
+            {
+                yield return allAnswers.Count;
+            }
         }
 
         private static IEnumerable<char[]> ReadGroupAnswers(string[] lines)
         {
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
 
-            foreach(var line in lines)
+            foreach (var line in lines)
             {
-                if(string.IsNullOrEmpty(line))
+                if (string.IsNullOrEmpty(line))
                 {
                     yield return sb.ToString().ToCharArray();
                     sb = new StringBuilder();
@@ -33,7 +90,7 @@ namespace Day6
                 }
                 sb.Append(line);
             }
-            if(sb.Length>0)
+            if (sb.Length > 0)
             {
                 yield return sb.ToString().ToCharArray();
             }
