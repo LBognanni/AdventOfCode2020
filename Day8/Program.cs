8d8bba77c40f596a58fd115cefc24a1027962ad3
Loris Bognanni <loris@codemade.co.uk>
2020-12-08 08:15:38 +0000

    Day 8 part 2 (brute force)

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -14,25 +14,52 @@ namespace Day8
         static void Main(string[] args)
         {
             var instructions = ParseInstructions("input.txt").ToArray();
-            var value = RunUntilRepeat(instructions);
+            var value = RunUntilRepeat(instructions).acc;
             Console.WriteLine($"Part 1: Accumulator value is {value}");
+
+            var value2 = BruteForce(instructions);
+            Console.WriteLine($"Part 2: Accumulator value is {value2}");
         }
 
-        private static int RunUntilRepeat(Instruction[] instructions)
+        private static int BruteForce(Instruction[] instructions)
+        {
+            for (int i = instructions.Length - 2; i >= 0; --i)
+            {
+                if (instructions[i].Type == "acc")
+                    continue;
+                var result = ReplaceAndRun(instructions, i);
+                if (!result.repeated)
+                    return result.acc;
+            }
+
+            return -1;
+        }
+
+        private static (int acc, bool repeated) ReplaceAndRun(Instruction[] instructions, int i)
+        {
+            var orig = instructions[i].Type;
+            var swap = orig == "jmp" ? "nop" : "jmp";
+            instructions[i] = instructions[i] with { Type = swap };
+            var result = RunUntilRepeat(instructions);
+            instructions[i] = instructions[i] with { Type = orig };
+            return result;
+        }
+
+        private static (int acc, bool repeated) RunUntilRepeat(Instruction[] instructions)
         {
             int acc = 0;
             int instructionIdx = 0;
             List<int> visitedIndices = new List<int>();
 
-            for(; ;)
+            while (instructionIdx < instructions.Length)
             {
                 if (visitedIndices.Contains(instructionIdx))
-                    return acc;
+                    return (acc, true);
 
                 var inst = instructions[instructionIdx];
                 visitedIndices.Add(instructionIdx);
 
-                switch(inst.Type)
+                switch (inst.Type)
                 {
                     case "acc":
                         acc += inst.Offset;
@@ -46,6 +73,8 @@ namespace Day8
                         break;
                 }
             }
+
+            return (acc, false);
         }
 
         private static IEnumerable<Instruction> ParseInstructions(string fileName)
