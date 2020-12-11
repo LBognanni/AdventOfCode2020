3edcce4a11dfb4785479b2fda9ad95aaf528b6b2
Loris Bognanni <loris@codemade.co.uk>
2020-12-11 11:20:21 +0000

    Day 11 part 2

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -7,19 +7,44 @@ namespace Day11
 {
     class Program
     {
+        private const char floor = '.';
+        private const char occupied = '#';
+        private const char free = 'L';
+
         static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
         {
             var input = File.ReadAllLines("input.txt").Select(l => l.ToCharArray()).ToArray();
 
             int mutations = 0;
-            while(Mutate(ref input))
+            while (Mutate(ref input))
             {
                 mutations++;
             }
-            Console.WriteLine($"Saw {mutations} mutations.");
+            Console.WriteLine($"Part 1: Saw {mutations} mutations.");
 
-            var occupied = input.SelectMany(l => l).Count(c => c == '#');
+            var occupied = input.SelectMany(l => l).Count(c => c == Program.occupied);
             Console.WriteLine($"Part 1: There are {occupied} occupied seats");
+        }
+
+        private static void Part2()
+        {
+            var input = File.ReadAllLines("input.txt").Select(l => l.ToCharArray()).ToArray();
+
+            int mutations = 0;
+            while (MutateV2(ref input))
+            {
+                mutations++;
+            }
+            Console.WriteLine($"Part 2: Saw {mutations} mutations.");
+
+            var occupied = input.SelectMany(l => l).Count(c => c == Program.occupied);
+            Console.WriteLine($"Part 2: There are {occupied} occupied seats");
         }
 
         private static bool Mutate(ref char[][] input)
@@ -34,8 +59,8 @@ namespace Day11
                 {
                     mutated[y][x] = input[y][x] switch
                     {
-                        'L' => CanOccupy(input, x, y),
-                        '#' => CanLeave(input, x, y),
+                        free => CanOccupy(input, x, y),
+                        occupied => CanLeave(input, x, y),
                         _ => input[y][x]
                     };
                     if (mutated[y][x] != input[y][x])
@@ -44,24 +69,46 @@ namespace Day11
                     }
                 }
             }
-            var s = PrettyPrint(mutated);
+
+            input = mutated;
+
+            return hasMutated;
+        }
+        private static bool MutateV2(ref char[][] input)
+        {
+            bool hasMutated = false;
+            var mutated = new char[input.Length][];
+
+            for (var y = 0; y < input.Length; ++y)
+            {
+                mutated[y] = new char[input[y].Length];
+                for (var x = 0; x < input[y].Length; ++x)
+                {
+                    mutated[y][x] = input[y][x] switch
+                    {
+                        free => CanOccupyV2(input, x, y),
+                        occupied => CanLeaveV2(input, x, y),
+                        _ => input[y][x]
+                    };
+                    if (mutated[y][x] != input[y][x])
+                    {
+                        hasMutated = true;
+                    }
+                }
+            }
 
             input = mutated;
 
             return hasMutated;
         }
 
-        private static IEnumerable<string> PrettyPrint(char[][] mutated)
-        {
-            foreach(var line in mutated)
-            {
-                yield return new string(line);
-            }
-        }
+        private static char CanLeaveV2(char[][] input, int x, int y) => NumberOf(input, x, y, occupied) > 4 ? free : occupied;
 
-        private static char CanLeave(char[][] input, int x, int y) => AtLeast(input, x, y, 4, '#') ? 'L' : '#';
+        private static char CanOccupyV2(char[][] input, int x, int y) => NumberOf(input, x, y, occupied) == 0 ? occupied : free;
 
-        private static char CanOccupy(char[][] input, int x, int y) => AtLeast(input, x, y, 8, 'L', '.') ? '#' : 'L';
+        private static char CanLeave(char[][] input, int x, int y) => AtLeast(input, x, y, 4, occupied) ? free : occupied;
+
+        private static char CanOccupy(char[][] input, int x, int y) => AtLeast(input, x, y, 8, free, floor) ? occupied : free;
 
         private static bool AtLeast(char[][] input, int x, int y, int n, params char[] find)
         {
@@ -81,18 +128,49 @@ namespace Day11
             return cells.Where(c => find.Contains(c)).Count() >= n;
         }
 
+
+        private static int NumberOf(char[][] input, int x, int y, char find)
+        {
+            var cells = new[]
+            {
+                ValueOrDefault(input, x, y, -1, -1),
+                ValueOrDefault(input, x, y, -1, 0),
+                ValueOrDefault(input, x, y, -1, 1),
+
+                ValueOrDefault(input, x, y, 0, -1),
+                ValueOrDefault(input, x, y, 0, 1),
+
+                ValueOrDefault(input, x, y, 1, -1),
+                ValueOrDefault(input, x, y, 1, 0),
+                ValueOrDefault(input, x, y, 1, +1),
+            };
+            return cells.Where(c => find == c).Count();
+        }
+
+        private static char ValueOrDefault(char[][] input, int x, int y, int dx, int dy)
+        {
+            while (y >= 0 && y < input.Length && x >= 0 && x < input[y].Length)
+            {
+                x += dx;
+                y += dy;
+                var c = ValueOrDefault(input, x, y);
+                if(c!= floor)
+                    return c;
+            }
+            return floor;
+        }
+
+
         private static char ValueOrDefault(char[][] input, int x, int y)
         {
-            const char defaultValue = '.';
-
             if (x < 0)
-                return defaultValue;
+                return floor;
             if (y < 0)
-                return defaultValue;
+                return floor;
             if (y >= input.Length)
-                return defaultValue;
+                return floor;
             if (x >= input[y].Length)
-                return defaultValue;
+                return floor;
 
             return input[y][x];
         }
