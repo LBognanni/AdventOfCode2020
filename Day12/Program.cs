1846e80ded1992752e819c49ec7db62dec5834a8
Loris Bognanni <loris@codemade.co.uk>
2020-12-12 10:04:11 +0000

    Day 12 part 2

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -7,7 +7,7 @@ namespace Day12
 {
     class Program
     {
-        record Ferry(int X, int Y, char Direction)
+        record Ferry(int X, int Y, int WayX, int WayY, char Direction)
         {
             public int Distance => (Math.Abs(X) + Math.Abs(Y));
         }
@@ -19,8 +19,8 @@ namespace Day12
             private static Dictionary<char, Func<Ferry, int, Ferry>> moves = new Dictionary<char, Func<Ferry, int, Ferry>>
             {
                 ['N'] = (f, d) => (f with { Y = f.Y - d }),
-                ['E'] = (f, d) => (f with { X = f.X - d }),
-                ['W'] = (f, d) => (f with { X = f.X + d }),
+                ['E'] = (f, d) => (f with { X = f.X + d }),
+                ['W'] = (f, d) => (f with { X = f.X - d }),
                 ['S'] = (f, d) => (f with { Y = f.Y + d }),
             };
             private static Dictionary<char, char> Opposites = new Dictionary<char, char>
@@ -68,27 +68,87 @@ namespace Day12
             private Ferry Rotate(Ferry ferry, Dictionary<char, char> rotations, int value)
             {
                 var direction = ferry.Direction;
-                while(value > 0)
+                while (value > 0)
                 {
                     direction = rotations[direction];
                     value -= 90;
                 }
                 return ferry with { Direction = direction };
             }
+
+            private static Dictionary<char, Func<Ferry, int, Ferry>> waypointMoves = new Dictionary<char, Func<Ferry, int, Ferry>>
+            {
+                ['N'] = (f, d) => (f with { WayY = f.WayY - d }),
+                ['E'] = (f, d) => (f with { WayX = f.WayX + d }),
+                ['W'] = (f, d) => (f with { WayX = f.WayX - d }),
+                ['S'] = (f, d) => (f with { WayY = f.WayY + d }),
+            };
+
+            public Ferry ApplyV2(Ferry ferry)
+            {
+                if (waypointMoves.ContainsKey(Action))
+                    return waypointMoves[Action](ferry, Value);
+
+                switch (Action)
+                {
+                    case 'F':
+                        return ferry with { X = ferry.X + (ferry.WayX * Value), Y = ferry.Y + (ferry.WayY * Value) };
+                    case 'B':
+                        return ferry with { X = ferry.X - (ferry.WayX * Value), Y = ferry.Y - (ferry.WayY * Value) };
+                    case 'L':
+                        return RotateWaypointLeft(ferry);
+                    case 'R':
+                        return RotateWaypointRight(ferry);
+                }
+
+                return ferry;
+            }
+
+            private Ferry RotateWaypointLeft(Ferry ferry)
+            {
+                int value = Value;
+                while (value > 0)
+                {
+                    ferry = ferry with { WayX = ferry.WayY, WayY = -ferry.WayX };
+                    value -= 90;
+                }
+                return ferry;
+            }
+            private Ferry RotateWaypointRight(Ferry ferry)
+            {
+
+                int value = Value;
+                while (value > 0)
+                {
+                    ferry = ferry with { WayX = -ferry.WayY, WayY = ferry.WayX };
+                    value -= 90;
+                }
+                return ferry;
+            }
         }
 
         static void Main(string[] args)
         {
             //var input = "F10\nN3\nF7\nR90\nF11".Split('\n').Select(Command.Parse).ToArray();
+
             var input = File.ReadAllLines("input.txt").Select(Command.Parse);
-            var ferry = new Ferry(0, 0, 'E');
+            var ferry = new Ferry(0, 0, 10, -1, 'E');
 
             foreach(var command in input)
             {
                 ferry = command.Apply(ferry);
             }
 
-            Console.WriteLine($"Ferry is at {ferry.X}, {ferry.Y} facing {ferry.Direction}. The Manhattan distance is { ferry.Distance }");
+            Console.WriteLine($"Part 1: Ferry is at {ferry.X}, {ferry.Y} facing {ferry.Direction}. The Manhattan distance is { ferry.Distance }");
+
+            ferry = new Ferry(0, 0, 10, -1, 'E');
+
+            foreach (var command in input)
+            {
+                ferry = command.ApplyV2(ferry);
+            }
+
+            Console.WriteLine($"Part 2: Ferry is at {ferry.X}, {ferry.Y}. The Manhattan distance is { ferry.Distance }");
         }
     }
 }
