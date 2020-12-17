059289bba06c2382f2cb79054ebf67d2531ffc16
Loris Bognanni <loris@codemade.co.uk>
2020-12-17 09:16:08 +0000

    Day 17 part 2

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -3,47 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-var initialState = File.ReadAllLines("input.txt").SelectMany((l, y) => l.Select((c, x) => new Cube(x, y, 0, c == '#')));
+var initialState = File.ReadAllLines("input.txt").SelectMany((l, y) => l.Select((c, x) => new Cube(new Position(x, y, 0, 0), c == '#')));
 var finalState = RunCycles(initialState, 6);
 
-Console.WriteLine($"Part 1: There are { finalState.Count(x=>x.Active) } active cubes");
+Console.WriteLine($"Part 2: There are { finalState.Count(x=>x.Active) } active cubes");
 
 
 
 IReadOnlyCollection<Cube> RunCycles(IEnumerable<Cube> cubes, int cycles)
 {
-    var newState = new Dictionary<(int, int, int), Cube>();
-    var initialState = cubes.ToDictionary(c => (c.X, c.Y, c.Z));
-    int minX = 0, maxX = cubes.Max(c => c.X), 
-        minY = 0, maxY = cubes.Max(c => c.Y), 
-        minZ = 0, maxZ = 0;
-    
+    var newState = new Dictionary<Position, Cube>();
+    var initialState = cubes.Where(c => c.Active).ToDictionary(c => c.Position);
+    var minPos = new Position(0, 0, 0, 0);
+    var maxPos = new Position(cubes.Max(c => c.Position.X), cubes.Max(c => c.Position.Y), 0, 0);
+
     for (int iCycle = 0; iCycle < cycles; ++iCycle)
     {
-        newState = new Dictionary<(int, int, int), Cube>();
-        minX--;
-        maxX++;
-        minY--;
-        maxY++;
-        minZ--;
-        maxZ++;
+        newState = new Dictionary<Position, Cube>();
+        minPos = minPos with { X = minPos.X - 1, Y = minPos.Y - 1, Z = minPos.Z - 1, W = minPos.W - 1 };
+        maxPos = maxPos with { X = maxPos.X + 1, Y = maxPos.Y + 1, Z = maxPos.Z + 1, W = maxPos.W + 1 };
 
-        for (int x = minX; x <= maxX; ++x)
+        for (int x = minPos.X; x <= maxPos.X; ++x)
         {
-            for (int y = minY; y <= maxY; ++y)
+            for (int y = minPos.Y; y <= maxPos.Y; ++y)
             {
-                for (int z = minZ; z <= maxZ; ++z)
+                for (int z = minPos.Z; z <= maxPos.Z; ++z)
                 {
-                    var cube = GetCubeAt(x, y, z, initialState);
-                    var activeNeighbours = GetNeighbours(cube, initialState).Count(x => x.Active);
+                    for (int w = minPos.W; w <= maxPos.W; ++w)
+                    {
+                        var pt = new Position(x, y, z, w);
+                        var cube = GetCubeAt(pt, initialState);
+                        var activeNeighbours = GetNeighbours(pt, initialState).Count(x => x.Active);
 
-                    if ((!cube.Active && activeNeighbours == 3) || (cube.Active && ((activeNeighbours == 2 || activeNeighbours == 3))))
-                    {
-                        newState.Add((x, y, z), new Cube(x, y, z, true));
-                    }
-                    else
-                    {
-                        newState.Add((x, y, z), new Cube(x, y, z, false));
+                        if ((!cube.Active && activeNeighbours == 3) || (cube.Active && ((activeNeighbours == 2 || activeNeighbours == 3))))
+                        {
+                            newState.Add(pt, new Cube(pt, true));
+                        }
+                        
                     }
                 }
             }
@@ -55,7 +51,7 @@ IReadOnlyCollection<Cube> RunCycles(IEnumerable<Cube> cubes, int cycles)
     return newState.Values;
 }
 
-IEnumerable<Cube> GetNeighbours(Cube pt, Dictionary<(int X, int Y, int Z), Cube> initialState)
+IEnumerable<Cube> GetNeighbours(Position pt, Dictionary<Position, Cube> initialState)
 {
     for (int x = pt.X - 1; x <= pt.X + 1; ++x)
     {
@@ -63,22 +59,27 @@ IEnumerable<Cube> GetNeighbours(Cube pt, Dictionary<(int X, int Y, int Z), Cube>
         {
             for (int z = pt.Z - 1; z <= pt.Z + 1; ++z)
             {
-                if ((x, y, z) != (pt.X, pt.Y, pt.Z))
+                for (int w = pt.W - 1; w <= pt.W + 1; ++w)
                 {
-                    yield return GetCubeAt(x, y, z, initialState);
+                    var pos = new Position(x, y, z, w);
+                    if (pt != pos)
+                    {
+                        yield return GetCubeAt(pos, initialState);
+                    }
                 }
             }
         }
     }
 }
 
-Cube GetCubeAt(int x, int y, int z, Dictionary<(int X, int Y, int Z), Cube> initialState)
+Cube GetCubeAt(Position pt, Dictionary<Position, Cube> initialState)
 {
-    if (initialState.TryGetValue((x, y, z), out var cube))
+    if (initialState.TryGetValue(pt, out var cube))
     {
         return cube;
     }
-    return new Cube(x, y, z, false);
+    return new Cube(pt, false);
 }
 
-record Cube(int X, int Y, int Z, bool Active);
+record Position (int X, int Y, int Z, int W);
+record Cube(Position Position, bool Active);
